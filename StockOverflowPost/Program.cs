807f60bd72cbbdb1e5aa6595ff2b2d103226bcfc
Burak Sen <s1127973@student.windesheim.nl>
2019-02-22 19:33:13 +0100

    first commit

--- StockOverflowPost/Program.cs
+++ StockOverflowPost/Program.cs
@@ -10,6 +10,42 @@ namespace StockOverflowPost
     {
         static void Main(string[] args)
         {
+            var post = new Post("Navid has a question", "Navid asked the next question: How can I change a private property?");
+
+            post.UpVote();
+            post.UpVote();
+            post.UpVote();
+            post.DownVote();
+            
+            Console.WriteLine(post.Title);
+            Console.WriteLine(post.Description);
+            Console.WriteLine(post.Vote);
+            Console.Read();
+        }
+    }
+
+    class Post
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public DateTime DateCreated { get; private set; }
+        public int Vote { get; set; }
+
+        public Post(string title, string description)
+        {
+            Title = title;
+            Description = description;
+            DateCreated = DateTime.Now;
+        }
+
+        public void UpVote()
+        {
+            this.Vote++;
+        }
+
+        public void DownVote()
+        {
+            this.Vote--;
         }
     }
 }
