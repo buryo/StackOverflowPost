a602201ed81c12d9f32be88889df9e560d883ad9
Burak Sen <s1127973@student.windesheim.nl>
2019-02-22 20:09:14 +0100

    separated the Post class from program.cs

---
+++ StockOverflowPost/Post.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace StockOverflowPost
+{
+    class Post
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public DateTime DateCreated { get; private set; }
+        public int Vote { get; set; }
+
+        public Post(string title, string description)
+        {
+            Title = title;
+            Description = description;
+            DateCreated = DateTime.Now;
+        }
+
+        public void UpVote()
+        {
+            this.Vote++;
+        }
+
+        public void DownVote()
+        {
+            this.Vote--;
+        }
+    }
+}
--- StockOverflowPost/Program.cs
+++ StockOverflowPost/Program.cs
@@ -23,29 +23,4 @@ namespace StockOverflowPost
             Console.Read();
         }
     }
-
-    class Post
-    {
-        public string Title { get; set; }
-        public string Description { get; set; }
-        public DateTime DateCreated { get; private set; }
-        public int Vote { get; set; }
-
-        public Post(string title, string description)
-        {
-            Title = title;
-            Description = description;
-            DateCreated = DateTime.Now;
-        }
-
-        public void UpVote()
-        {
-            this.Vote++;
-        }
-
-        public void DownVote()
-        {
-            this.Vote--;
-        }
-    }
 }
